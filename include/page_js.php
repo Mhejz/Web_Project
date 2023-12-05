81efcf32241ae5df3d2b0be4d57b96b8743a406a
mhejz <mhejz1310@gmail.com>
2023-12-05 12:57:57 +0800

    add jquery

--- include/page_js.php
+++ include/page_js.php
@@ -1,2 +1,5 @@
+<!-- JQUERY -->
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+
 <!-- SWEETALERT 2  -->
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
