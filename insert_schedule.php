97f6c9c4f61d4f407dbd4b462651fc3d8f09a512
mhejz <mhejz1310@gmail.com>
2023-12-05 12:38:46 +0800

    add form to insert schedule

--- insert_schedule.php
+++ insert_schedule.php
@@ -10,7 +10,19 @@
     <title>Document</title>
 </head>
 <body>
-    <h1 class="display-1 text-center">Insert schedule</h1>
+    <div class="container">
+        <h1 class="display-1 text-center">Insert schedule</h1>
+
+        <br>
+        <a href="../index.php" class="btn bg-secondary border-3 border-secondary-subtle mt-3 fw-bold text-light" style="border-radius: 20px; width: 100px;">back</a>
+        <br><br>
+        <form action="">
+            <input type="text" id="" name="" class="form-control" placeholder="dito mag insert ng data">
+            <br>
+            <button type="submit" class="btn btn-success">Add Schedule</button>
+        </form>
+    </div>
+
     <!-- BOOSTRAP JS -->
     <script src="assets/js/bootstrap.min.js"></script>
 </body>
