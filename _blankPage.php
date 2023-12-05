b61591da010c6d834603eb9add630122e0c9300b
mhejz <mhejz1310@gmail.com>
2023-12-05 13:17:33 +0800

    add framework page

---
+++ _blankPage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- CONNECTION -->
+    <?php include('config/dbcon.php')?>
+    <!-- BOOTSTRAP 5 CSS -->
+    <?php include('include/page_header.php')?>
+    <!-- OTHER PLUGINS -->
+    <!-- CUSTOM CSS -->
+    <style></style>
+</head>
+<body>
+    <div class="container">
+        <h1 class="display-1 text-center">blank page</h1>
+    </div>
+    <!-- BOOTSTRAP 5 JS -->
+    <?php include('include/page_js.php')?>
+    <!-- OTHER PLUGINS -->
+    <!-- CUSTOM JS -->
+    <script></script>
+</body>
+</html>
