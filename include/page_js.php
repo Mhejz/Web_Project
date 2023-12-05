372b91a3fa3ae9911527f2b81acaf7dcaf5ccb1b
mhejz <mhejz1310@gmail.com>
2023-12-05 13:04:45 +0800

    js page add bootstrap js

--- include/page_js.php
+++ include/page_js.php
@@ -1,5 +1,9 @@
 <!-- JQUERY -->
 <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 
+<!-- BOOTSTRAP 5 JS -->
+<link rel="stylesheet" href="assets/js/bootstrap.min.js">
+
+<!-- OTHER PLUGINS JS -->
 <!-- SWEETALERT 2  -->
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
