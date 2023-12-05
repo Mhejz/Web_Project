4cec1f79c4d23aed8d99da3acebdf30395a68a82
mhejz <mhejz1310@gmail.com>
2023-12-05 12:22:23 +0800

    debug the connection

--- config/dbcon.php
+++ config/dbcon.php
@@ -6,10 +6,19 @@
 
     $conn = mysqli_connect($server, $host, $pass, $name);
 
-    if($conn){
-        die('Connection Error!').mysqli_connect_error();
+    if(!$conn){
+        // Log connection error to the PHP error log
+        error_log('Connection Error: ' . mysqli_connect_error());
+        
+        // Alternatively, you can use echo to print the error to the browser
+        // echo 'Connection Error: ' . mysqli_connect_error();
+
+        die('Connection Error!');
     }
     else{
+        // Log a success message to the PHP error log
+        error_log('Connected to the database');
+
         echo "connected";
     }
 ?>
